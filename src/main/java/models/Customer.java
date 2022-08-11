02861960fb06bad80d4ca1d94c02e0797f4a6b95
MagoroGraphics <magoro.graphics@gmail.com>
2022-08-11 13:34:48 +0100

    Custome created

---
+++ src/main/java/models/Customer.java
@@ -0,0 +1,19 @@
+package models;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+@Builder
+public class Customer {
+    @NonNull
+    private final String firstName;
+    @NonNull
+    private final String lastName;
+    @NonNull
+    private String town;
+    @NonNull
+    private int age;
+
+}
---
+++ src/test/java/models/CustomerTest.java
@@ -0,0 +1,40 @@
+package models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomerTest {
+    Customer customer1;
+
+    @Before
+    public void before(){
+        customer1 = Customer
+                .builder()
+                .firstName("John")
+                .lastName("Doe")
+                .town("Edinburgh")
+                .age(30)
+                .build();
+    }
+
+    @Test
+    public void getFirstName() {
+        assertEquals("John",customer1.getFirstName());
+    }
+
+    @Test
+    public void getLastName() {
+        assertEquals("Doe",customer1.getLastName());
+    }
+
+    @Test
+    public void getTown() {
+
+    }
+
+    @Test
+    public void getAge() {
+    }
+}
